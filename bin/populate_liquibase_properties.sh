04f8617e7323575bf1d3138bc241a6497ddefcb7
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-03 17:12:52 +0530

    Update populate_liquibase_properties.sh

--- bin/populate_liquibase_properties.sh
+++ bin/populate_liquibase_properties.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Preparing Liquibase Properties for DB Engine - $1 and DB Name - $2"
+
 echo driver: ${DB_DRIVER}	 	    > ./conf/$1/$2/liquibase.properties
 echo url: ${DB_JDBC_URL} 		    > ./conf/$1/$2/liquibase.properties
 echo username: ${DB_USERNAME} 	> ./conf/$1/$2/liquibase.properties
