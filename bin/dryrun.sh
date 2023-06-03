ffa9e647e78039fe2e8e0113efe55096db362de1
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-03 15:40:48 +0530

    Create dryrun.sh

---
+++ bin/dryrun.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+. bin/populate-liquibase-properties
+
+liquibase --defaults-file=${LIQUIBASE_PROPERTIES} --changeLogFile=${CHANGELOG} --log-level=info updateSQL
+
+if [ ${#} -eq 0 ]
+then
+	echo "Success: Liquibase Execution"
+else
+	echo "Failed: Liquibase Execution"
+	echo "Release Liquibase Lock"
+	./liquibase/liquibase --defaults-file=${LIQUIBASE_PROPERTIES} releaseLocks
+	exit 1 # wrong args
+fi
