7566d52a6d129e964f4a8d70818889d0814d4504
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-03 16:06:00 +0530

    Update populate_liquibase_properties.sh

--- bin/populate_liquibase_properties.sh
+++ bin/populate_liquibase_properties.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo driver: ${DB_DRIVER}	 	    > ./conf/$1/$2/liquibase.properties
+echo url: ${DB_JDBC_URL} 		    > ./conf/$1/$2/liquibase.properties
+echo username: ${DB_USERNAME} 	> ./conf/$1/$2/liquibase.properties
+echo password: ${DB_PASSWORD} 	> ./conf/$1/$2/liquibase.properties
+echo liquibaseSchemaName=public 		    > ./conf/$1/$2/liquibase.properties
