88c5390ce033f322092b8235acd1df2b87d8da1a
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-04 15:43:22 +0530

    Update get_secrets.py

--- bin/get_secrets.py
+++ bin/get_secrets.py
@@ -2,21 +2,13 @@ import boto3
 import json
 import argparse
 
-def main():
-    parser = argparse.ArgumentParser(description='Sample Python Script')
-    parser.add_argument('--arg1', help='Argument 1')
-
-    args = parser.parse_args()
-
-    print(f"Argument 1: {args.arg1}")
-
+def get_aws_secret(secret_id):
     client = boto3.client('secretsmanager')
-    response = client.get_secret_value(SecretId={args.arg1})
+    response = client.get_secret_value(SecretId=secret_id)
     secret_value = response['SecretString']
     secrets = json.loads(secret_value)
     print(secrets['host'])
     print(secrets['username'])
     print(secrets['password'])
 
-if __name__ == '__main__':
-    main()
+    return secret_value
