65862d7835bb72ee23697aaf2baf76128faf7821
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-04 22:34:12 +0530

    Update get_secrets.py

--- bin/get_secrets.py
+++ bin/get_secrets.py
@@ -1,8 +1,18 @@
-def get_secrets(skid,sk):
-    import boto3
-    import json
+import json
+import boto3
+
+def main(fn,skid):
     client = boto3.client('secretsmanager')
     response = client.get_secret_value(SecretId=skid)
     secret_value = response['SecretString']
     secrets = json.loads(secret_value)
-return secrets[sk]
+
+    file = open(fn, "w")
+    file.write("driver: org.postgresql.Driver\n")
+    file.write("username: +secrets['username']\n")
+    file.write("password: +secrets['password']\n")
+    file.write("url: +secrets['host']\n")
+    file.close()
+
+if __name__ == '__main__':
+    main(fn,skid)
