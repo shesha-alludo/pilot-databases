a1e3aeb51dc3c50d871270b7df20e4634d1d3d72
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-04 22:52:19 +0530

    Update get_secrets.py

--- bin/get_secrets.py
+++ bin/get_secrets.py
@@ -1,18 +1,15 @@
 import json
 import boto3
+import sys
 
-def main(fn,skid):
-    client = boto3.client('secretsmanager')
-    response = client.get_secret_value(SecretId=skid)
-    secret_value = response['SecretString']
-    secrets = json.loads(secret_value)
+client = boto3.client('secretsmanager')
+response = client.get_secret_value(SecretId=sys.argv[2])
+secret_value = response['SecretString']
+secrets = json.loads(secret_value)
 
-    file = open(fn, "w")
-    file.write("driver: org.postgresql.Driver\n")
-    file.write("username: +secrets['username']\n")
-    file.write("password: +secrets['password']\n")
-    file.write("url: +secrets['host']\n")
-    file.close()
-
-if __name__ == '__main__':
-    main(fn,skid)
+file = open(sys.argv[1], "w")
+file.write("driver: org.postgresql.Driver\n")
+file.write("username: +secrets['username']\n")
+file.write("password: +secrets['password']\n")
+file.write("url: +secrets['host']\n")
+file.close()
