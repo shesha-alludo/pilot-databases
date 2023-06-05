68a32afea1d131f3cd8bedadf83151a2792efd61
shesha-alludo <133131948+shesha-alludo@users.noreply.github.com>
2023-06-05 08:01:26 +0530

    Update run_liquibase.sh

--- bin/run_liquibase.sh
+++ bin/run_liquibase.sh
@@ -2,8 +2,9 @@
 echo "#!/bin/bash" > ./bin/lb_prep.sh
 for lbpf in ./conf/*/*/liquibase.properties
 do
-        export db_conf_home=echo $lbpf|awk  -F"/" '{"1/$2/$3"}'
-        echo $db_conf_home
-        echo "./liquibase/liquibase --defaults-file=$lbpf --changeLogFile=$db_conf_home/changelog.xml --log-level=debug updateSQL" >> ./bin/lb_prep.sh
+       export db_conf_home=`echo $lbpf|awk  -F"/" '{"1/$2/$3"}'`
+       echo $db_conf_home
+       echo "./liquibase/liquibase --defaults-file=$lbpf --changeLogFile=$db_conf_home/changelog.xml --log-level=debug updateSQL" >> ./bin/lb_prep.sh
+
 done
 cat ./bin/lb_prep.sh
